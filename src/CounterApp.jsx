673bbade6a4489b24362d0fe75bd9aaa353bfcfa
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-03-31 09:57:44 +0900

    CounterApp + (최대 10까지), if문, 삼항연산자, Math.min()

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -16,6 +16,16 @@ function CounterApp() {
       <button onClick={() => setCount((count) => count + 2)}>+ 2</button>
       <button onClick={() => setCount((count) => count - 1)}>-</button>
       <button onClick={() => setCount(0)}>reset</button>
+
+      <br />
+      
+      <button onClick={() => setCount((count) => {
+        if(count + 1 >= 10) return 10;
+        return count + 1;
+      })}>+ (최대 10까지)</button>
+      <button onClick={() => setCount((count) => count + 1 >= 10 ? 10 :count+1) }>+ (최대 10까지)</button>
+      <button onClick={() => setCount((count) => Math.min(count + 1, 10)) }>+ (최대 10까지)</button>
+
       {/* <button onClick={setCount(0)}>reset</button> Too many re-renders. */}
       {/* <button
         className="counter"

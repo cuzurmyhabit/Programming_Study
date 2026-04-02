2839bdccee7a239648ac7836e27f4642d7eb1e64
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-04-02 13:28:33 +0900

    CounterApp plusMax10

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -8,6 +8,12 @@ function CounterApp() {
   //count: 이전 또는 현재 값, setCount(): count값 변경 함수. 반드시 이걸 거쳐야 함.
   const [count, setCount] = useState(0)
 
+  function plusMax10() {
+    setCount((count) => {
+      if (count + 1 >= 10) return 10;
+      return count + 1;
+    })
+  }
   return (
     <>
 
@@ -18,14 +24,16 @@ function CounterApp() {
       <button onClick={() => setCount(0)}>reset</button>
 
       <br />
-      
+
       <button onClick={() => setCount((count) => {
-        if(count + 1 >= 10) return 10;
+        if (count + 1 >= 10) return 10;
         return count + 1;
       })}>+ (최대 10까지)</button>
-      <button onClick={() => setCount((count) => count + 1 >= 10 ? 10 :count+1) }>+ (최대 10까지)</button>
-      <button onClick={() => setCount((count) => Math.min(count + 1, 10)) }>+ (최대 10까지)</button>
+      <button onClick={() => setCount((count) => count + 1 >= 10 ? 10 : count + 1)}>+ (최대 10까지)</button>
+      <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+ (최대 10까지)</button>
 
+      <button onClick={() => plusMax10()}>+ (최대 10까지)</button>
+      {/* <button onClick={() => plusMax10}>+ (최대 10까지)</button> */}
       {/* <button onClick={setCount(0)}>reset</button> Too many re-renders. */}
       {/* <button
         className="counter"

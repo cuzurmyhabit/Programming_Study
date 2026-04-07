4a813234acbae6afd1896209d085e1c3421f18ae
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-04-07 09:03:54 +0900

    TodoListApp HTML className todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,30 +1,23 @@
-import { useState } from 'react';
+import "./todolist.css"
 
 function TodoListApp() {
     return (
-        <>
-            <h1>ToDoToDo</h1>
-            <form action="">
-                <input type="text" />
-                <button>Add</button>
-            </form>
+        <div className="todo">
+            <h1 className="todo__title">ToDoToDo</h1>
+            <form className="todo__form">
+                <input type="text" className='todo__input' placeholder='할 일을 입력하세용' />
+                <button type='submit' className='todo__button todo__button--add'>Add</button>
+            </form> 
 
-
-            <ul>
-                <li>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="">옷 싸기</label>
-                    <button>🤺</button>
-                    <button>🗑️</button>
-                </li>
-                <li>
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="">잠 자기</label>
-                    <button>🤺</button>
-                    <button>🗑️</button>
+            <ul className='todo__list'>
+                <li className='todo__item todo__item--complete'>
+                    <input type="checkbox" className='todo__check' name="" id="ch-1"/>
+                    <label htmlFor="ch-1" className='todo__label'>옷 싸기</label>
+                    <button className='todo__button todo__button--edit'>🤺</button>
+                    <button className='todo__button todo__button--delete'>🗑️</button>
                 </li>
             </ul>
-        </>
+        </div>
     );
 }
 

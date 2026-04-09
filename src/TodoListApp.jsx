0c2adf2e75c8da2d15ec0b06eebe4486f935ad34
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-04-09 14:49:03 +0900

    TodoListApp React Component 만들기: Button, Checkbox, TodoItemEmpty

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,4 +1,8 @@
+//https://programming-study.s2472.workers.dev/
 import "./todolist.css"
+import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
+import TodoItemEmpty from "./components/TodoItemEmpty";
 
 function TodoListApp() {
     return (
@@ -6,18 +10,16 @@ function TodoListApp() {
             <h1 className="todo__title">ToDoToDo</h1>
             <form className="todo__form">
                 <input type="text" className='todo__input' placeholder='할 일을 입력하세용' />
-                <button type='submit' className='todo__button todo__button--add'>Add</button>
+                <Button type='submit' className='todo__button todo__button--add'>Add</Button>
             </form> 
 
             <ul className='todo__list'>
-                <li className="todo__item todo__item--empty">
-                    <p>할 일 없음</p>
-                </li>
+                <TodoItemEmpty />
+                <li className="todo__item todo__item--empty"></li>
                 <li className='todo__item todo__item--complete'>
-                    <input type="checkbox" className='todo__check' name="" id="ch-1"/>
-                    <label htmlFor="ch-1" className='todo__label'>옷 싸기</label>
-                    <button className='todo__button todo__button--edit'>🤺</button>
-                    <button className='todo__button todo__button--delete'>🗑️</button>
+                    <Checkbox type="checkbox" className='todo__check' name="" id="ch-1"/>
+                    <Button className='todo__button todo__button--edit'>🤺</Button>
+                    <Button className='todo__button todo__button--delete'>🗑️</Button>
                 </li>
             </ul>
         </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+function Button(props) {
+    console.log(props)
+    const { children, ...rest } = props;
+
+    return (
+        <button {...rest}>{children}</button>
+    );
+}
+
+export default Button;

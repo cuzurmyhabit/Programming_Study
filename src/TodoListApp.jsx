5aaf5773305f095ae446438f1a2a3642a422ad36
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-04-14 10:05:31 +0900

    TodoListAPp addTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,14 +1,35 @@
 //https://programming-study.s2472.workers.dev/
 import "./todolist.css"
+import { useState } from "react";
 import TodoHeader from "./components/TodoHeader";
 import TodoAdder from "./components/TodoAdder";
 import TodoList from "./components/TodoList";
 
+class Todo{
+    constructor(id, text, isCompleted){
+        this.id = id;
+        this.text = text;
+        this.isCompleted = isCompleted;
+    }
+}
 function TodoListApp() {
+
+    const [todos, setTodos] = useState([]);
+    function addTodo(text){
+        // 이전 todos에 newTodo 만들어서 추가하자 -> 그것을 setTodos로 하자
+        setTodos((todos) => [
+            ...todos,   //todos에 있는 item을 다 꺼내서 새로운 리스트에 하나씩 넣자
+            new Todo(
+                Date.now(), // 고유 ID 시간을 이용 == new Date().getTime()
+                text,   // 할 일 내용
+                false    // 완료 여부
+            )
+        ]);
+    }
     return (
         <div className="todo">
             <TodoHeader />
-            <TodoAdder />
+            <TodoAdder addTodo={addTodo} />
             <TodoList />
         </div>
     );
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,9 +1,22 @@
 import Button from "./Button";
+import { useState } from "react";
 
-export default function TodoAdder() {
+export default function TodoAdder( {addTodo} ) {    // const addTodo = props.addTodo; -> 구조분해할당
+    const [inputTodo, setInputTodo] = useState("");
+    const handleSubmit = (event) => {
+        addTodo(inputTodo);   // input에 있는 text value를 할일내용으로 추가
+        
+    }
+    
     return (
-        <form className="todo__form">
-                <input type="text" className='todo__input' placeholder='할 일을 입력하세용' />
+        <form className="todo__form" onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    className='todo__input' 
+                    placeholder='할 일을 입력하세용' 
+                    value={inputTodo}
+                    onChange={(event) => setInputTodo(event.target.value)}
+                />
                 <Button type='submit' className='todo__button todo__button--add'>Add</Button>
             </form> 
     )

a136ddfe053fcaf450f10d4adee50bc7be31063e
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-04-21 10:10:27 +0900

    TodoListApp toggleTodo()

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,7 @@
 import TodoItemEmpty from "./TodoItemEmpty";
 import TodoItem from "./TodoItem";
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, toggleTodo }) {
     return (
         //todo.isCompleted이 true면 todo__item--complete, false ""
         <ul className={`todo__item ${todos.some(t => t.isCompleted) ? 'todo__item--complete' : ""}`}>
@@ -10,7 +10,7 @@ export default function TodoList({ todos }) {
             {/* todos가 있으면 todoItem에 todos 던지기 */}
             {todos.length > 0 &&
              todos.map((todo) => (
-                 <TodoItem key={todo.id} todo={todo} />
+                 <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
              ))}
             {/* todos에서 하나씩 꺼내서 todo -> <TodoItem todo={todo} /> */}
         </ul>
